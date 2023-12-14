d2c93d20a9be804bcb29c31a2b082789fb842717
MaarikaK <testmaarika@gmail.com>
2023-12-15 00:06:16 +0200

    week7

---
+++ Week7 Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+int a = 5;
+int b = 6;
+
+Add(b, a);
+Add(10, 100);
+Add(637, 6278);
+static void Add(int x, int y)
+{
+    Console.WriteLine($"{x} + {y} = {x + y}");
+}
+
+multiply(b, a);
+multiply(10, 100);
+multiply(637, 6278);
+static void multiply(int x, int y)
+{
+    Console.WriteLine($"{x} * {y} = {x * y}");
+}
+
+substract(b, a);
+substract(100, 10);
+substract(55, 11);
+
+
+static void substract(int x, int y)
+{
+    Console.WriteLine($"{x} - {y} = {x - y}");
+}
+
+divide(b, a);
+divide(100, 10);
+divide(55, 11);
+static void divide(int x, int y)
+{
+    Console.WriteLine($"{x} / {y} = {x / y}");
+}
---
+++ Week7_Functions/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+string hello = "Hello, world!";
+string panic = "Don´t panic";
+
+PrintStringFiveTimes(panic);
+PrintStringFiveTimes(hello);
+PrintStringFiveTimes("Maarika");
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine(hello);
+//}
+
+//for (int i = 0;i < 5; i++)
+//{
+//    Console.WriteLine(panic);
+//}
+
+static void PrintStringFiveTimes(string someString)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(someString);
+    }
+}
